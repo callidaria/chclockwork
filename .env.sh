2081aafd06e1382bfd23707157026b37e43dc6d2
callidaria <lo.true@gmx.de>
2025-08-05 02:21:01 +0200

    fixing setup script layout

--- .env.sh
+++ .env.sh
@@ -35,20 +35,20 @@ chcw_setup()
 
 	# windows library warnings
 	elif [ "$OS" == "Windows_NT" ]; then
-	        echo "WARNING: you are running the auto-setup on windows. Trying to auto-setup with MSYS2!"
-	        pacman -S --noconfirm \
-		       mingw-w64-x86_64-gcc \
-		       mingw-w64-x86_64-make \
-		       mingw-w64-x86_64-glew \
-		       mingw-w64-x86_64-SDL2 \
-		       mingw-w64-x86_64-glm \
-		       mingw-w64-x86_64-assimp \
-		       mingw-w64-x86_64-freetype
+		echo "WARNING: you are running the auto-setup on windows. Trying to auto-setup with MSYS2!"
+		pacman -S --noconfirm \
+			   mingw-w64-x86_64-gcc \
+			   mingw-w64-x86_64-make \
+			   mingw-w64-x86_64-glew \
+			   mingw-w64-x86_64-SDL2 \
+			   mingw-w64-x86_64-glm \
+			   mingw-w64-x86_64-assimp \
+			   mingw-w64-x86_64-freetype
 
 	# exotic people know whats next - setup yourself i don't know your ways
 	else
-	    echo "ERROR: distro not natively supported!"
-	    echo "please install libs on your own & submit what's missing."
+		echo "ERROR: distro not natively supported!"
+		echo "please install libs on your own & submit what's missing."
 	fi
 
 	# texture headers
@@ -80,7 +80,7 @@ alias r='make release'
 alias ra='make release -B'
 
 if [ "$OS" == "Windows_NT" ]; then
-    alias e="./chcw.exe"
+	alias e="./chcw.exe"
 else
-    alias e="./chcw"
+	alias e="./chcw"
 fi
